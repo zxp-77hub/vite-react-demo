3cb1dc131555b3470bdce0c3cbc613a5e09a414e
xuepeng.zhang <xuepeng.zhang@77hub.com>
2024-05-09 17:38:18 +0800

    [todo list demo]-00000-task-Todo list 持久化数据梳理

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,18 @@
 // import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import sotre from '@/store'
+import sotre,{persistor} from '@/store'
 import { Provider } from "react-redux";
+import {PersistGate} from 'redux-persist/integration/react'
+import App from './App.tsx'
+
 import './index.scss'
 import '@/styles/common.scss'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   // <React.StrictMode> // 取消严格模式
   <Provider store={sotre}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
   // </React.StrictMode>,
 )
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,29 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore,combineReducers } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import todoReducer from "./todoInfo";
 
-const store = configureStore({
-  reducer:{
-    todoInfo:todoReducer
-  }
-  
+
+const rootReducer = combineReducers({
+  todoInfo:todoReducer
 })
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig,rootReducer)
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware:(getDefaultMiddleware)=>
+    getDefaultMiddleware({
+      serializableCheck:false, //开发中避免不必要警告
+    }),
+})
+
+export const persistor = persistStore(store)
 export default  store
 
 export type Store = ReturnType<typeof store.getState>
