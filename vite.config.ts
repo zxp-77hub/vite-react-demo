278e7a95835c867209c95864bdaa5c43e6372de3
xuepeng.zhang <xuepeng.zhang@77hub.com>
2024-05-10 17:58:42 +0800

    [react demo]-task-0000-mixin全局样式通过配置引入

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-// import sassPlugin from 'vite-plugin-sass'
+// import scss from 'vite-plugin-sass'
 import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css:{
+    preprocessorOptions:{
+      scss:{
+        additionalData: '@import "@/styles/mixin.scss";',
+      }
+    }
+  },
   server:{
     port:3000, // 端口
     open:true, // 项目运行时自动打开浏览器
